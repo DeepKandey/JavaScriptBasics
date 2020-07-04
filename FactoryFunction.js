ac19f282921e2cc0cc123e49eb108287ccf3f9ef
DeepKandey <deepakggsipu@gmail.com>
2020-07-04 13:39:26 +0530

    Factory and Constructor Functions examples

---
+++ FactoryFunction.js
@@ -0,0 +1,42 @@
+console.log("----------Normal Object Creation-----------");
+// normal object create
+const circle1 = {
+    radius: 1,
+    isVisible: true,
+    location: {
+        x: 1,
+        y: 1
+    },
+
+    draw: function () {
+        console.log("draw function")
+    }
+}
+
+console.log(circle1);
+
+console.log("\n----------Factory Function-----------");
+// object creation using Factory Function
+function createCircle(radius) {
+    return {
+        radius: radius,
+        draw() {
+            console.lo("draw function using factory function");
+        }
+    }
+}
+
+const c1 = createCircle(1);
+console.log(c1);
+
+console.log("\n----------Constructor Function-----------");
+// constructor function example
+function CreateCircles(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log("draw function using constructor function")
+    }
+}
+
+const c2 = new CreateCircles(2)
+console.log(c2)
