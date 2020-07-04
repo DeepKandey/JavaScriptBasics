6edfd8c2e99d4cd16813e6e02f7cdcc00cb8d4ef
DeepKandey <deepakggsipu@gmail.com>
2020-07-04 20:44:50 +0530

    ValueAndReference Concept and Dynamic Object Concept

---
+++ ValueAndRefType.js
@@ -0,0 +1,14 @@
+
+let p = 10;
+let q = 20;
+p = 30;
+console.log(p);
+console.log(q);
+
+console.log("Reference Type");
+let a = { value: 10 };
+let b = a;
+a.value = 30;
+console.log(a);
+console.log(b);
+
